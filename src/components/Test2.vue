799c687caf8393be9ec0708cd7f47dc17fb173a4
Chung <753cindy@naver.com>
2020-02-16 12:45:37 +0900

    first commit

---
+++ src/components/Test2.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        {{msg}}
+    </div>
+</template>
+
+<script>
+    import EventBus from '../main';
+    export default  {
+        data(){
+            return {
+                msg: '',
+                count: 0,
+            }
+        },
+        created() {
+            EventBus.$on('push-msg', (payload) => {
+                this.msg = payload;
+                this.count++;
+            })
+        }
+    }
+</script>
